17f82904cfb873240fa4ada34701758968129fbf
SumanDas3001 <sumandas3001@gmail.com>
2022-01-24 11:38:49 +0530

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,20 +64,19 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data["email"]).first rescue nil
+    user = User.where(email: data["email"]).first
 
-    if user != nil && !user.present?
-      user = User.create(
-        email: data["email"],
-        password: Devise.friendly_token[0, 20]
-      )
-  
-      user.name = access_token.info.name
-      user.image = access_token.info.image
-      user.provider = access_token.provider
-      user.uid = access_token.uid
-      user.save
-    end
+    user ||= User.create(
+      email: data["email"],
+      password: Devise.friendly_token[0, 20]
+    )
+
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.save
+
     user
   end
 
