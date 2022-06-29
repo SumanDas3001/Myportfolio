427869bc5545c2d461159529aaa91a31bcb6f60e
SumanDas3001 <sumandas3001@gmail.com>
2022-06-29 23:10:28 +0530

    Change footer design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,82 +1,77 @@
 module ApplicationHelper
-	def login_helper style = " "
+  def login_helper(style = ' ')
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style )
+      (link_to 'Register', new_user_registration_path, class: style) +
+        ' '.html_safe +
+        (link_to 'Login', new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
-	### Content tag
-	def source_helper(styles)
+  ### Content tag
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to 'contact me',
+                                                                                                 contact_path} if you'd like to work together."
       content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
-	#### Copyright
-	def copyright_generator
-		CodecampViewTool::Render.copyright 'DasDevGuide', 'All right reserved'
-	end
+  #### Copyright
+  def copyright_generator
+    CodecampViewTool::Render.copyright 'DasDevGuide', 'All right reserved'
+  end
 
-	def nav_items
-		[
-			# {
-			# 	url: root_path,
-			# 	title: 'Home'
-			# },
-			# {
-			# 	url: about_me_path,
-			# 	title: 'About Me'
-			# },
-			# {
-			# 	url: contact_path,
-			# 	title: 'Contact'
-			# },
-			{
-				url: blogs_path,
-				title: 'Blogs'
-			},
-			# {
-			# 	url: own_portfolios_path,
-			# 	title: 'Portfolio'
-			# },
-			{
-				url: tech_news_path,
-				title: 'Tech News'
-			}
-		]
-	end
+  def nav_items
+    [
+      # {
+      # 	url: root_path,
+      # 	title: 'Home'
+      # },
+      # {
+      # 	url: about_me_path,
+      # 	title: 'About Me'
+      # },
+      # {
+      # 	url: contact_path,
+      # 	title: 'Contact'
+      # },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      }
+      # {
+      # 	url: own_portfolios_path,
+      # 	title: 'Portfolio'
+      # },
+      # {
+      # 	url: tech_news_path,
+      # 	title: 'Tech News'
+      # }
+    ]
+  end
 
-	def nav_helper  style, tag_type
+  def nav_helper(style, tag_type)
+    nav_links = ''
 
-		nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
 
-		nav_items.each do |item|
-			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
-		end
+    nav_links.html_safe
+  end
 
-		nav_links.html_safe
-	end
+  def active?(path)
+    'active' if current_page? path
+  end
 
-	def active? path
-		"active" if current_page? path
-	end
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    alert_generator alert if alert
+  end
 
-	def alerts
-		alert = (flash[:alert] || flash[:error] || flash[:notice])
-		if alert
-			alert_generator alert
-		end
-	end
-
-	def alert_generator msg
-		js add_gritter(msg, title: "DasDevGuide", sticky: false)
-	end
+  def alert_generator(msg)
+    js add_gritter(msg, title: 'DasDevGuide', sticky: false)
+  end
 end
-
-
-
